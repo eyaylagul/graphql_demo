853f2674e9b054cdac74800114b35d9d6eaeee42
Viktor Denisov <websitevirtuoso@gmail.com>
2019-06-17 15:04:06 -0700

    Add ide-helper, telescope, laratrust

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,16 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      *
      * @return void
@@ -24,5 +14,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 }
---
+++ config/laratrust_seeder.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'role_structure'  => [
+        'administrator' => [
+            'user'       => 'c,r,u,up',
+            'post'       => 'c,r,u,d',
+            'category'   => 'c,r,u,d',
+            'role'       => 'c,r,u,d',
+            'permission' => 'r',
+            'city'       => 'c,r,u,d',
+            'state'      => 'c,r,u,d',
+            'country'    => 'r',
+            'property'   => 'c,r,u,d',
+        ],
+        'manager'       => [
+            'user'     => 'r,u',
+            'post'     => 'r,u',
+            'category' => 'r,u',
+            'city'     => 'r',
+            'state'    => 'r',
+            'country'  => 'r',
+            'property' => 'c,r,u,d',
+        ],
+        'editor'        => [
+            'user'     => 'r,u',
+            'post'     => 'r,u',
+            'category' => 'r,u',
+            'property' => 'r',
+        ],
+    ],
+//    'permission_structure' => [
+//        'admin' => [
+//            'user' => 'c,r,u,d',
+//        ],
+//    ],
+    'permissions_map' => [
+        'c'  => 'create',
+        'r'  => 'view',
+        'u'  => 'update',
+        'd'  => 'delete',
+        'up' => 'update.password'
+    ]
+];
